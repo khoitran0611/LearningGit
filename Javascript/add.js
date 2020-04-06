337031ed886ae184902d87cf4a3a657696503005
Khoi Tran <ktran0611@gmail.com>
2020-04-06 17:45:09 +0700

    add code

--- Javascript/add.js
+++ Javascript/add.js
@@ -1,5 +1,9 @@
 function add(operand1, operand2) {
     //add 2 operands to get the sum
     //Remove unused rtrtrtrtrt
-    return operand1 + operand2; 
+    if (operand1 === undefined || operand2 === undefined) {
+        return 0;
+    }
+
+    return  parseFloat(operand1) + parseFloat(operand2); 
 }
